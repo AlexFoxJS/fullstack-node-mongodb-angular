a2bd963d1dda82b9b300d72804ecf49fc01740bf
AlexFoxJS <alexfoxjs@gmail.com>
2018-10-14 00:31:29 +0300

    master - reinstall client site of application

--- client/e2e/src/app.e2e-spec.ts
+++ client/e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to fullstack-node-mongodb-angular!');
+    expect(page.getParagraphText()).toEqual('Welcome to client!');
   });
 });
