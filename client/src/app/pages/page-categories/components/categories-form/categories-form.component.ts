af4eb008a17ba91b4478eb85d7333d5c83eb4cca
AlexFoxJS <alexfoxjs@gmail.com>
2018-10-21 00:56:15 +0300

    master - lesson 47 - finalize page create/update category logic

--- client/src/app/pages/page-categories/components/categories-form/categories-form.component.ts
+++ client/src/app/pages/page-categories/components/categories-form/categories-form.component.ts
@@ -1,5 +1,5 @@
 /** Библиотеки - Системные */
-import {Component, OnInit} from '@angular/core'
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core'
 import {ActivatedRoute, Params} from '@angular/router'
 import {FormControl, FormGroup, Validators} from '@angular/forms'
 
@@ -16,6 +16,9 @@ import {CategoriesService} from '../../../../shared/services/categories.service'
 /** Классы */
 import {MaterialService} from '../../../../shared/classes/material.service'
 
+/** Интерфейс */
+import {Category} from '../../../../shared/interfaces/category'
+
 @Component({
   selector: 'app-categories-form',
   templateUrl: './categories-form.component.html',
@@ -25,7 +28,12 @@ export class CategoriesFormComponent implements OnInit {
 
   public form: FormGroup
   public isNew: boolean = true
+  public image: File
+  public imagePreview: string = ''
+  public category: Category
   private readonly notifier: NotifierService
+
+  @ViewChild('inputFile') inputFile: ElementRef
 
   constructor(
     private route: ActivatedRoute,
@@ -35,6 +43,7 @@ export class CategoriesFormComponent implements OnInit {
     this.notifier = notifierService
   }
 
+  /** */
   ngOnInit() {
 
     this.form = new FormGroup({
@@ -42,15 +51,6 @@ export class CategoriesFormComponent implements OnInit {
     })
 
     this.form.disable()
-
-    // this.route.params.subscribe((params: Params) => {
-    //   if (params.id) {
-    //     // Редактирование
-    //     this.isNew = false
-    //   } else {
-    //     // Создание
-    //   }
-    // })
 
     this.route.params.pipe(
       switchMap(
@@ -64,25 +64,64 @@ export class CategoriesFormComponent implements OnInit {
         }
       )
     )
-      .subscribe(
-        res => {
-          if (res) {
-            this.form.patchValue({
-              name: res.name
-            })
-            MaterialService.updateTextInput()
-          }
-          this.form.enable()
-        },
-        error => {
-          this.notifier.notify('error', error.error.message)
+    .subscribe(
+      (res: Category) => {
+        if (res) {
+          this.category = res
+          this.form.patchValue({
+            name: res.name
+          })
+          this.imagePreview = res.imageURL
+          MaterialService.updateTextInput()
         }
-      )
+
+        this.form.enable()
+      },
+      error => {
+        this.notifier.notify('error', error.error.message)
+      }
+    )
 
   }
 
-  onSubmit() {
+  /** */
+  triggerInputFileClick() {
+    this.inputFile.nativeElement.click()
+  }
 
+  /** */
+  onFileUpload(event: any) {
+    const file = event.target.files[0]
+    this.image = file
+
+    const reader = new FileReader()
+
+    reader.onload = () => {
+      this.imagePreview = reader.result
+    }
+
+    reader.readAsDataURL(file)
+  }
+
+  /** */
+  onSubmit() {
+    let formSubmitSubscribe$
+    this.form.disable()
+
+    if (this.isNew) {
+      formSubmitSubscribe$ = this.categoriesService.createCategory(this.form.value.name, this.image)
+    } else {
+      formSubmitSubscribe$ = this.categoriesService.updateCategory(this.category._id, this.form.value.name, this.image)
+    }
+
+    formSubmitSubscribe$.subscribe(res => {
+      this.category = res
+      this.notifier.notify('success', 'Данные успешно отправлены')
+      this.form.enable()
+    }, error => {
+      this.notifier.notify('error', 'Ошибка при отправке формы')
+      this.form.enable()
+    })
   }
 
 }
--- client/src/app/shared/services/categories.service.ts
+++ client/src/app/shared/services/categories.service.ts
@@ -8,7 +8,6 @@ import {Observable} from 'rxjs'
 /** */
 import {Category} from '../interfaces/category'
 
-
 @Injectable({
   providedIn: 'root'
 })
@@ -18,12 +17,36 @@ export class CategoriesService {
   constructor(private http: HttpClient) {
   }
 
+  /** */
   fetchCategories(): Observable<Category[]> {
     return this.http.get<Category[]>('/api/category')
   }
 
+  /** */
   getCategoryById(id: string): Observable<Category> {
     return this.http.get<Category>(`/api/category/${id}`)
+  }
+
+  /** */
+  createCategory(name: string, image?: File): Observable<Category> {
+    const formData = new FormData()
+
+    if (image) formData.append('image', image, image.name)
+
+    formData.append('name', name)
+
+    return this.http.post<Category>('/api/category', formData)
+  }
+
+  /** */
+  updateCategory(id: string, name: string, image?: File): Observable<Category> {
+    const formData = new FormData()
+
+    if (image) formData.append('image', image, image.name)
+
+    formData.append('name', name)
+
+    return this.http.patch<Category>(`/api/category/${id}`, formData)
   }
 
 }
