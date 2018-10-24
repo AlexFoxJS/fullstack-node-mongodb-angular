abd0ce15fd2d82a1a09ce1be7c52a9666d5957ee
AlexFoxJS <alexfoxjs@gmail.com>
2018-10-24 08:33:56 +0300

    master - lesson 53 - improved orderPosition logic and edit order modal

--- client/src/app/pages/page-order/page-order.component.ts
+++ client/src/app/pages/page-order/page-order.component.ts
@@ -1,18 +1,58 @@
 /** Системные библиотеки */
-import {Component, OnInit} from '@angular/core'
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core'
+import {NavigationEnd, Router} from '@angular/router'
+
+/** Сервисы */
+import {MaterialInstance, MaterialService} from '../../shared/classes/material.service'
+import {OrderService} from './services/order.service'
 
 @Component({
   selector: 'app-page-order',
   templateUrl: './page-order.component.html',
-  styleUrls: ['./page-order.component.css']
+  styleUrls: ['./page-order.component.css'],
+  providers: [OrderService]
 })
 
-export class PageOrderComponent implements OnInit {
+export class PageOrderComponent implements OnInit, OnDestroy, AfterViewInit {
 
-  constructor() {
+  @ViewChild('modalExploreOrder') modalExploreOrderRef: ElementRef
+  public modalExploreOrder: MaterialInstance
+  public isRoot: boolean
+
+  constructor(
+    private router: Router,
+    private order: OrderService
+  ) {
   }
 
   ngOnInit() {
+    this.isRoot = this.router.url === '/order'
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        console.log('event', event)
+        this.isRoot = this.router.url === '/order'
+      }
+    })
+  }
+
+  ngOnDestroy() {
+    this.modalExploreOrder.destroy()
+  }
+
+  ngAfterViewInit() {
+    this.modalExploreOrder = MaterialService.initModal(this.modalExploreOrderRef)
+  }
+
+  openModalExploreOrder() {
+    this.modalExploreOrder.open()
+  }
+
+  cancelModalExploreOrder() {
+    this.modalExploreOrder.close()
+  }
+
+  submitModalExploreOrder() {
+
   }
 
 }
