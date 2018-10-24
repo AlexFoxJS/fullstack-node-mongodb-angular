563e485074c355d78ee853239936c680a73db9ed
AlexFoxJS <alexfoxjs@gmail.com>
2018-10-24 10:27:12 +0300

    master - lesson 55 - improved orderService

--- client/src/app/pages/page-order/services/order.service.ts
+++ client/src/app/pages/page-order/services/order.service.ts
@@ -3,21 +3,45 @@ import {Injectable} from '@angular/core'
 
 /** Интерфейсы */
 import {Position} from '../../../shared/interfaces/position'
+import {OrderPosition} from '../../../shared/interfaces/order-position'
 
 @Injectable()
 
 export class OrderService {
 
+  public list: OrderPosition[] = []
+  public price = 0
+
   add(position: Position) {
-    console.log(position)
+    const orderPosition: OrderPosition = {
+      name: position.name,
+      cost: position.cost,
+      quantity: position.quantity,
+      _id: position._id
+    }
+    const candidate = this.list.find(p => p._id === position._id)
+
+    if (candidate) candidate.quantity += orderPosition.quantity
+    else this.list.push(orderPosition)
+
+    this.recalculatePrices()
   }
 
-  remove(position: Position) {
+  remove(orderPosition: OrderPosition) {
+    const orderPositionIndex = this.list.findIndex(p => p._id === orderPosition._id)
 
+    this.list.splice(orderPositionIndex, 1)
+    this.recalculatePrices()
   }
 
   clear() {
 
+  }
+
+  private recalculatePrices() {
+    this.price = this.list.reduce((totalPrice, item) => {
+      return totalPrice += item.cost * item.quantity
+    }, 0)
   }
 
 }
