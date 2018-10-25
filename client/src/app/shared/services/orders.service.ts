046fe9fa16dada1309f2e46ef151808ace95e223
AlexFoxJS <alexfoxjs@gmail.com>
2018-10-25 10:24:14 +0300

    master - lesson 56 - create new serviceOrder

---
+++ client/src/app/shared/services/orders.service.ts
@@ -0,0 +1,26 @@
+/** */
+import {Injectable} from '@angular/core'
+import {HttpClient} from '@angular/common/http'
+
+/** */
+import {Observable} from 'rxjs'
+
+/** */
+import {Order} from '../interfaces/order'
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class OrdersService {
+
+  constructor(
+    private http: HttpClient
+  ) {
+  }
+
+  create(order: Order): Observable<Order> {
+    return this.http.post<Order>('/api/order/', order)
+  }
+
+}
