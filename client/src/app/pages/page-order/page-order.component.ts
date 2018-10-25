3d373a40eef14253bf79230b1748b1fe708ed0f8
AlexFoxJS <alexfoxjs@gmail.com>
2018-10-25 10:25:33 +0300

    master - lesson 56 - improve logic post new orders

--- client/src/app/pages/page-order/page-order.component.ts
+++ client/src/app/pages/page-order/page-order.component.ts
@@ -2,12 +2,20 @@
 import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core'
 import {NavigationEnd, Router} from '@angular/router'
 
+/** */
+import {Subscription} from 'rxjs'
+
+/** */
+import {NotifierService} from 'angular-notifier'
+
 /** Сервисы */
 import {MaterialInstance, MaterialService} from '../../shared/classes/material.service'
 import {OrderService} from './services/order.service'
 
 /** Интерфейсы */
 import {OrderPosition} from '../../shared/interfaces/order-position'
+import {OrdersService} from '../../shared/services/orders.service'
+import {Order} from '../../shared/interfaces/order'
 
 @Component({
   selector: 'app-page-order',
@@ -19,13 +27,20 @@ import {OrderPosition} from '../../shared/interfaces/order-position'
 export class PageOrderComponent implements OnInit, OnDestroy, AfterViewInit {
 
   @ViewChild('modalExploreOrder') modalExploreOrderRef: ElementRef
+
   public modalExploreOrder: MaterialInstance
   public isRoot: boolean
+  public pending = false
+  public subscribeCreateOrder: Subscription
+  private readonly notifier: NotifierService
 
   constructor(
     public order: OrderService,
     private router: Router,
+    private ordersService: OrdersService,
+    private notifierService: NotifierService,
   ) {
+    this.notifier = notifierService
   }
 
   ngOnInit() {
@@ -39,25 +54,60 @@ export class PageOrderComponent implements OnInit, OnDestroy, AfterViewInit {
 
   ngOnDestroy() {
     this.modalExploreOrder.destroy()
+
+    if (this.subscribeCreateOrder) {
+      this.subscribeCreateOrder.unsubscribe()
+    }
   }
 
   ngAfterViewInit() {
     this.modalExploreOrder = MaterialService.initModal(this.modalExploreOrderRef)
   }
 
+  /** */
   openModalExploreOrder() {
     this.modalExploreOrder.open()
   }
 
+  /** */
   cancelModalExploreOrder() {
+    this.closeModalExploreOrder()
+  }
+
+  /** */
+  closeModalExploreOrder() {
     this.modalExploreOrder.close()
   }
 
+  /** */
   removePosition(orderPosition: OrderPosition) {
     this.order.remove(orderPosition)
   }
 
+  /** */
   submitModalExploreOrder() {
+    const order: Order = {
+      list: this.order.list.map(item => {
+        delete item._id
+        return item
+      })
+    }
+
+    this.pending = true
+
+    this.subscribeCreateOrder = this.ordersService.create(order).subscribe(
+      newOrder => {
+        this.notifier.notify('success', `Заказ №${newOrder.order} был добавлен.`)
+        this.order.clear()
+      },
+      error => {
+        this.notifier.notify('error', `${error.error.message}`)
+      },
+      () => {
+        this.closeModalExploreOrder()
+        this.pending = false
+      }
+    )
 
   }
 
