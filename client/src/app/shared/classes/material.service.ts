5a7c2aade6319730b9347169c5ff8835d638b845
AlexFoxJS <alexfoxjs@gmail.com>
2018-10-28 18:39:31 +0300

    master - lesson 61 - create initDatepicker method in MaterialInstance class

--- client/src/app/shared/classes/material.service.ts
+++ client/src/app/shared/classes/material.service.ts
@@ -1,4 +1,5 @@
 import {ElementRef, Injectable} from '@angular/core'
+import {MaterialDatepicker} from "../interfaces/material-datepicker";
 
 declare var M
 
@@ -32,6 +33,40 @@ export class MaterialService {
   /** */
   static initTooltip(ref: ElementRef) {
     return M.Tooltip.init(ref.nativeElement)
+  }
+
+  /** */
+  static initDatepicker(ref: ElementRef, onClose: () => void): MaterialDatepicker {
+    return M.Datepicker.init(ref.nativeElement, {
+      format: 'dd.mm.yyyy',
+      showClearBtn: true,
+      onClose,
+      // i18n: {
+      //   monthsShort: [
+      //     'янв',
+      //     'фев',
+      //     'мар',
+      //     'апр',
+      //     'май',
+      //     'июн',
+      //     'июл',
+      //     'авг',
+      //     'сен',
+      //     'окт',
+      //     'ноя',
+      //     'дек'
+      //   ],
+      //   weekdaysShort: [
+      //     'Вс',
+      //     'Пн',
+      //     'Вт',
+      //     'Ср',
+      //     'Чт',
+      //     'Пт',
+      //     'Сб'
+      //   ]
+      // }
+    })
   }
 
 }
