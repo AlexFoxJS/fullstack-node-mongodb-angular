19dab54e4bbaa64224d33966ba22d261b01c6f28
AlexFoxJS <alexfoxjs@gmail.com>
2018-10-28 15:11:26 +0300

    master - lesson 59 - add fetch orders method on the client

--- client/src/app/shared/services/orders.service.ts
+++ client/src/app/shared/services/orders.service.ts
@@ -1,6 +1,6 @@
-/** */
+/**   */
 import {Injectable} from '@angular/core'
-import {HttpClient} from '@angular/common/http'
+import {HttpClient, HttpParams} from '@angular/common/http'
 
 /** */
 import {Observable} from 'rxjs'
@@ -19,8 +19,18 @@ export class OrdersService {
   ) {
   }
 
+  /** */
   create(order: Order): Observable<Order> {
     return this.http.post<Order>('/api/order/', order)
+  }
+
+  /** */
+  fetch(params: any = {}): Observable<Order[]> {
+    return this.http.get<Order[]>('api/order/', {
+      params: new HttpParams({
+        fromObject: params
+      })
+    })
   }
 
 }
