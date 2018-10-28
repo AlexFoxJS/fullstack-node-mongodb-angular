625d94c0766454aecb7eb07bd3210a4611ebf6dd
AlexFoxJS <alexfoxjs@gmail.com>
2018-10-28 13:55:24 +0300

    master - lesson 57 - create pageHistory component

--- client/src/app/pages/page-history/page-history.component.ts
+++ client/src/app/pages/page-history/page-history.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+/** */
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core'
+
+/** */
+import {MaterialInstance, MaterialService} from "../../shared/classes/material.service";
 
 @Component({
   selector: 'app-page-history',
   templateUrl: './page-history.component.html',
   styleUrls: ['./page-history.component.css']
 })
-export class PageHistoryComponent implements OnInit {
 
-  constructor() { }
+export class PageHistoryComponent implements OnInit, OnDestroy, AfterViewInit {
+
+  @ViewChild('filterTooltip') filterTooltipRef: ElementRef
+  public filterTooltip: MaterialInstance
+  public isFilterVisible: boolean = false
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  ngOnDestroy() {
+    this.filterTooltip.destroy()
+  }
+
+  ngAfterViewInit() {
+    this.filterTooltip = MaterialService.initTooltip(this.filterTooltipRef)
+  }
+
 }
+
