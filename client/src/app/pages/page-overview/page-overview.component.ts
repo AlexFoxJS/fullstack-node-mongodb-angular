01df154ac2848fc1fd21ff508aafca2306aae3a4
AlexFoxJS <alexfoxjs@gmail.com>
2018-10-28 20:45:51 +0300

    master - lesson 63 - improve pageOverview component logic

--- client/src/app/pages/page-overview/page-overview.component.ts
+++ client/src/app/pages/page-overview/page-overview.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core'
+/** Модули - Системные */
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core'
+
+/** Модули - Дочерние */
+import {Observable} from "rxjs";
+
+/** Сервисы */
+import {AnalyticsService} from '../../shared/services/analytics.service'
+import {MaterialInstance, MaterialService} from '../../shared/classes/material.service'
+
+/** Интерфейсы */
+import {OverviewPage} from '../../shared/interfaces/overview-page'
 
 @Component({
   selector: 'app-page-overview',
@@ -6,11 +17,35 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./page-overview.component.css']
 })
 
-export class PageOverviewComponent implements OnInit {
+export class PageOverviewComponent implements OnInit, OnDestroy, AfterViewInit {
 
-  constructor() { }
+  @ViewChild('tapTarget') tapTargetRef: ElementRef
+
+  public data$: Observable<OverviewPage>
+  public tapTarget: MaterialInstance
+  public yesterday: Date = new Date()
+
+  constructor(
+    private analyticsService: AnalyticsService
+  ) {
+  }
 
   ngOnInit() {
+    this.data$ = this.analyticsService.getOverviews()
+    this.yesterday.setDate(this.yesterday.getDate() - 1)
+  }
+
+  ngOnDestroy() {
+    this.tapTarget.destroy()
+  }
+
+  ngAfterViewInit() {
+    this.tapTarget = MaterialService.initTapTarget(this.tapTargetRef)
+  }
+
+  /** */
+  showInfo() {
+    this.tapTarget.open()
   }
 
 }
