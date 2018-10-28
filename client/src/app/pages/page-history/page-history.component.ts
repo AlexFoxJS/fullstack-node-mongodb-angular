3c1a32722e4ee8c38bbe71c2b66abe14818d8835
AlexFoxJS <alexfoxjs@gmail.com>
2018-10-28 15:13:17 +0300

    master - lesson 59 - improved pageHistory component logic

--- client/src/app/pages/page-history/page-history.component.ts
+++ client/src/app/pages/page-history/page-history.component.ts
@@ -2,7 +2,22 @@
 import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core'
 
 /** */
-import {MaterialInstance, MaterialService} from "../../shared/classes/material.service";
+import {Subscription} from 'rxjs'
+
+/** */
+import {NotifierService} from 'angular-notifier'
+
+/**  */
+import {MaterialInstance, MaterialService} from '../../shared/classes/material.service'
+
+/** */
+import {OrdersService} from '../../shared/services/orders.service'
+
+/** */
+import {Order} from '../../shared/interfaces/order'
+
+/** */
+const FILTER_STEP = 2
 
 @Component({
   selector: 'app-page-history',
@@ -13,21 +28,67 @@ import {MaterialInstance, MaterialService} from "../../shared/classes/material.s
 export class PageHistoryComponent implements OnInit, OnDestroy, AfterViewInit {
 
   @ViewChild('filterTooltip') filterTooltipRef: ElementRef
+
   public filterTooltip: MaterialInstance
   public isFilterVisible: boolean = false
+  public subscription: Subscription
+  public offset: number = 0
+  public limit: number = FILTER_STEP
+  public orders: Order[] = []
+  public loading: boolean = false
+  public reloading: boolean = false
+  public noMoreOrders: boolean = false
 
-  constructor() {
+  private readonly notifier: NotifierService
+
+  constructor(
+    private ordersService: OrdersService,
+    private notifierService: NotifierService,
+  ) {
+    this.notifier = notifierService
   }
 
   ngOnInit() {
+    this.reloading = true
+    this.fetchOrders()
   }
 
   ngOnDestroy() {
     this.filterTooltip.destroy()
+    this.subscription.unsubscribe()
   }
 
   ngAfterViewInit() {
     this.filterTooltip = MaterialService.initTooltip(this.filterTooltipRef)
+  }
+
+  /** Метод получения списка заказов */
+  private fetchOrders() {
+    const params = {
+      offset: this.offset,
+      limit: this.limit
+    }
+
+    this.subscription = this.ordersService.fetch(params).subscribe(
+      orders => {
+        this.orders = this.orders.concat(orders)
+        this.noMoreOrders = FILTER_STEP > orders.length
+      },
+      error => {
+        this.notifier.notify('error', `${error.error.message}`)
+      },
+      () => {
+        this.loading = false
+        this.reloading = false
+      }
+    )
+  }
+
+  /** Метод получения дополнительных заказов */
+  loadMoreOrders() {
+    this.offset += FILTER_STEP
+    this.fetchOrders()
+    this.loading = true
   }
 
 }
