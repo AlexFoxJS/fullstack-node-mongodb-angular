e00c6290ae19c86328e7018dcf3a8072dfdfff35
AlexFoxJS <alexfoxjs@gmail.com>
2018-10-28 22:50:27 +0300

    master - lesson 64 - create new interfaces analyticsPage and analyticsChartItem

---
+++ client/src/app/shared/interfaces/analytics-chart-item.ts
@@ -0,0 +1,6 @@
+/** */
+export interface AnalyticsChartItem {
+  gain: number,
+  order: number,
+  label: string,
+}
---
+++ client/src/app/shared/interfaces/analytics-page.ts
@@ -0,0 +1,7 @@
+/** Интерфейсы */
+import {AnalyticsChartItem} from './analytics-chart-item'
+
+export interface AnalyticsPage {
+  averageCheck: number,
+  chart: AnalyticsChartItem
+}
