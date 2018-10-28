dabd307e238cdb38dce304139f32cccb92217ad7
AlexFoxJS <alexfoxjs@gmail.com>
2018-10-28 16:59:27 +0300

    master - lesson 60 - improve historyList component logic

--- client/src/app/pages/page-history/components/history-list/history-list.component.ts
+++ client/src/app/pages/page-history/components/history-list/history-list.component.ts
@@ -1,8 +1,11 @@
 /** */
-import {Component, Input, OnInit} from '@angular/core'
+import {AfterViewInit, Component, ElementRef, Input, OnDestroy, ViewChild} from '@angular/core'
 
 /** */
 import {Order} from '../../../../shared/interfaces/order'
+
+/** */
+import {MaterialInstance, MaterialService} from '../../../../shared/classes/material.service'
 
 @Component({
   selector: 'app-history-list',
@@ -10,14 +13,38 @@ import {Order} from '../../../../shared/interfaces/order'
   styleUrls: ['./history-list.component.css']
 })
 
-export class HistoryListComponent implements OnInit {
+export class HistoryListComponent implements OnDestroy, AfterViewInit {
 
   @Input() orders: Order[] = []
+  @ViewChild('modalOrder') modalOrderRef: ElementRef
 
-  constructor() {
+  modalOrder: MaterialInstance
+  selectedOrder: Order
+
+  ngOnDestroy() {
+    this.modalOrder.destroy()
   }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    this.modalOrder = MaterialService.initModal(this.modalOrderRef)
+  }
+
+  /** Подсчет общей суммы заказа */
+  computeOrderPrice(order: Order): number {
+    return order.list.reduce((total, item): number => {
+      return total += item.quantity * item.cost
+    }, 0)
+  }
+
+  /** */
+  selectOrder(order: Order) {
+    this.selectedOrder = order
+    this.modalOrder.open()
+  }
+
+  /** */
+  closeModalOrder() {
+    this.modalOrder.close()
   }
 
 }
