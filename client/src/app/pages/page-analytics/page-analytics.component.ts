b9c79aeff2f86509277ccb9746ec6ae10c657de3
AlexFoxJS <alexfoxjs@gmail.com>
2018-10-28 22:52:29 +0300

    master - lesson 65 - improve pageAnalytics component logic

--- client/src/app/pages/page-analytics/page-analytics.component.ts
+++ client/src/app/pages/page-analytics/page-analytics.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+/** Модули - Системные */
+import {AfterViewInit, Component, ElementRef, OnDestroy, ViewChild} from '@angular/core'
+
+/** Модули - Дочерние */
+import {Subscription} from 'rxjs'
+
+/** Сервисы */
+import {AnalyticsService} from '../../shared/services/analytics.service'
+
+/** Интерфейсы */
+import {AnalyticsPage} from '../../shared/interfaces/analytics-page'
 
 @Component({
   selector: 'app-page-analytics',
   templateUrl: './page-analytics.component.html',
   styleUrls: ['./page-analytics.component.css']
 })
-export class PageAnalyticsComponent implements OnInit {
+export class PageAnalyticsComponent implements AfterViewInit, OnDestroy {
 
-  constructor() { }
+  @ViewChild('gainChart') gainChartRef: ElementRef
+  @ViewChild('orderChart') orderChartRef: ElementRef
 
-  ngOnInit() {
+  public subscription: Subscription
+  public averageCheck: number = 0
+  public pending: boolean = true
+
+  constructor(
+    private analyticsService: AnalyticsService
+  ) {
+  }
+
+  ngAfterViewInit() {
+    this.subscription = this.analyticsService.getAnalytics().subscribe((data: AnalyticsPage) => {
+      // TODO: Удалить "mock" данные после появления настоящих данных для отрисвки графиков в базе
+      const mockData = {
+        averageCheck: 1355,
+        chart: [
+          {
+            gain: 500,
+            order: 1,
+            label: '03.10.2018',
+          },
+          {
+            gain: 855,
+            order: 2,
+            label: '07.10.2018',
+          },
+        ]
+      }
+
+      this.averageCheck = mockData.averageCheck
+      this.pending = false
+    })
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe()
+    }
   }
 
 }
